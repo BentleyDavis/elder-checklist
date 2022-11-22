70cd03a2d859d940bf3e45e71de2db107daa2314
Bentley Davis <Bentley@BentleyDavis.com>
2022-11-22 11:45:34 -0600

    tweak

--- src/components/Components.tsx
+++ src/components/Components.tsx
@@ -7,7 +7,6 @@ import ToDo from "./Todo";
 import Reminder from "./Reminder";
 import Select from "./Select";
 import Events from "./Events";
-import { lowerFirst } from "lodash";
 
 const components: { [key: string]: any } = {
     matrix: Matrix,
